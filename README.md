21755797d17b8b885c9e9cfb39fb27189475ccd6
DY Lim <dlagus6412@gmail.com>
2022-12-14 05:23:47 +0000

    Final Crit

--- README.md
+++ README.md
@@ -1,29 +1,231 @@
-### ClothSense
-##### CASA0016
----
+# ClothSense
 
-Factors
-- Time
+CASA0016
+
+DY Lim
+
+* * *
+
+  
+
+![](http://localhost:9425/images/c1b11536-7deb-4060-b856-c49acf1203f5.jpeg)
+
+## When is it going to be dried?
+
+###   
+
+### No dryer.
+
+Energy Bill, Damage on cloth, Noise...
+
+  
+
+Every time I needed to touch the cloth to check whether it is dried or still wet.
+
+However, it is **_hard to predict_** even if i do so. 
+
+Also, I want to **_boost the drying process_** and want to **_know the condition_** where it is located(Bad air quality, weather).
+
+  
+
+ClothSense is going to  **monitor**:
+
 - Temperature
 - Humidity
-- Air Quality
-- Light
-- etc
+- ~~Air Quality~~
 
-## Overall
-![](./resource/Overall.png)
+ClothSense is going to **boost**:
 
-## Diagram
-![Project Diagram](./resource/wire_28Nov.png)
+- Dry State
 
-### Sensor Communication Protocol
-- LED Matrix : SPI
-- bme680 : I2C
-- AS7341 : I2C
+ClothSense is going to **show:**
 
-![Arduino Leonardo Pinout](./resource/pinout_arduinoLeonardo.png)
+- Estimated Time to dry
+- Current Environmental Condition
 
-## LoRaWAN
-- Long-Range Transmissions
-- Low Power
-- The Things Network
+###   
+
+## Component
+
+- Arduino Uno (MCU)
+- ~~BME680~~ DHT22 (Environment Sensor) - Digital Pin
+- max7219 LED Matrix 8x32 (Display) - SPI
+- NF-F12 5V PMW (Fan) - PWM
+- Buttons - Digital Pin
+
+  
+
+## Workflow
+
+![](http://localhost:9425/images/65b1f9c9-5059-4b16-b5ae-42253287b557.png)
+
+  
+
+## Build Process
+
+### First Prototype
+
+![](http://localhost:9425/images/a965379b-d051-42b9-ae5d-6bfdb4b226b5.png)  
+
+Tested Fan Control using PWM control
+
+By pressing button, Uno changes fan speed for noise control.
+
+![](http://localhost:9425/images/55688d8f-8f8c-4ddb-87db-3514510ee90c.png)  
+
+[Arduino PWM Tutorial - Arduino Project Hub](https://create.arduino.cc/projecthub/muhammad-aqib/arduino-pwm-tutorial-ae9d71)  
+
+  
+
+### Second Prototype
+
+![](http://localhost:9425/images/1213f4df-0544-4aa9-a8c4-c65307eda173.png)  
+
+[Old-fashioned vintage radio isolated on a white background - | CanStock (canstockphoto.com)](https://www.canstockphoto.com/old-fashioned-vintage-radio-isolated-on-82587771.html)  
+
+Inspired by the old fashion radios, got the idea from Prof. Andy.
+
+  
+
+![](http://localhost:9425/images/982c5723-b4ff-43a7-ab0e-ef83bd6093bc.png)  
+
+3D Printing Enclosure
+
+- Slow
+- Not Precise
+- Good for 3d solid object prototyping
+
+Removed servo
+
+Moved LED Matrix
+
+Changed sie
+
+  
+
+### Third Prototype
+
+![](http://localhost:9425/images/abc93835-26d8-4c76-951c-06ebfb5f4913.png)  
+
+![](http://localhost:9425/images/d88af8ba-775a-4ee6-9d4f-5a50a94d498d.jpeg)  
+
+Laser cut wood enclosure
+
+- Fast
+- Precise
+
+3D Printed top lid to hide LED matrix and for aesthetic reason
+
+  
+
+with the enclosure, the device was tested and some problems are found:
+
+1. BME680 library uses more than 30% of maximum sketch storage. (From 70% to 117%)
+
+- [Reduce the size and memory usage of your sketch – Arduino Help Center](https://support.arduino.cc/hc/en-us/articles/360013825179-Reduce-the-size-and-memory-usage-of-your-sketch)  
+    
+- Remove BME680 and use  DHT22
+- Use Arduino Uno instead of Leonardo (LoRa Library)
+- Optimised variables and functions
+
+3. Did not design the component mounting
+
+- Made some mounting brackets
+- ![](http://localhost:9425/images/5f301b1e-915b-435e-95f6-9ef12db263cf.jpeg)  
+    
+
+  
+
+### Final Build
+
+Modularised parts
+
+![](http://localhost:9425/images/1ce0a8f7-6a10-4c30-b78e-c392fd43dba1.jpeg) ![](http://localhost:9425/images/1c0e3eff-6608-427f-9409-922dc34b161a.jpeg)
+
+  
+
+Used DHT22
+
+![](http://localhost:9425/images/ed67920e-300b-444d-84e0-961939e7e633.jpeg)  
+
+![](http://localhost:9425/images/4cb51150-4156-4f55-b198-d3ff9d0d71b2.jpeg)  
+
+  
+
+Added mesh to prevent the interference between the fan and wires
+
+![](http://localhost:9425/images/bf99227b-b9bc-420b-a301-27ff90cd2668.jpeg)  
+
+![](http://localhost:9425/images/81d22b71-f546-4fe2-87cd-4b94b15b7248.jpeg)  
+
+  
+
+Introduced the mounting bracket
+
+![](http://localhost:9425/images/097a3f5d-d074-4a8a-9289-28d31aa242a5.jpeg)  
+
+  
+
+Result
+
+![](http://localhost:9425/images/bd999468-23cf-47e6-8c8c-8b88cf63810d.jpeg)  
+
+![](http://localhost:9425/images/a7b3d021-064b-484e-85ee-26fd9b698d2b.png)  
+
+![](http://localhost:9425/images/50d484b1-130d-4fef-9fc4-0c2241c8223c.jpeg)  
+
+![](http://localhost:9425/images/b55368e4-0d01-4e3d-b6f8-7eba9f8418dd.jpeg)  
+
+![](http://localhost:9425/images/c73377bb-a535-409a-b1ba-858350322714.jpeg)  
+
+  
+
+## If i had more time, I would
+
+- Use wireless data transmission such as WiFi, LoRa...
+- Improve the enclosure
+- Order a custom PCB board to reduce the device size
+
+  
+
+## Where can it be used?
+
+![](http://localhost:9425/images/71460ba0-4807-41e9-a745-6ed08437ac9c.png)  
+
+[A Grade Dehydrated Vegetables, Pan India, Packaging Size: 5 Kg at Rs 350/kg in New Delhi (indiamart.com)](https://www.indiamart.com/proddetail/dehydrated-vegetables-24169149248.html)  
+
+![](http://localhost:9425/images/750f08aa-71a3-4f84-9b5e-4d7b0796a330.png)  
+
+[9 Hat and Coat Storage Ideas When You Don't Have a Coat Closet | Apartment Therapy](https://www.apartmenttherapy.com/9-creative-ways-to-organize-your-coats-and-hats-by-the-door-when-you-dont-have-a-coat-closet-239859)  
+
+  
+
+## Lesson Learnt
+
+- Test, Test, Test and Test.
+
+- Even though the several design, Some improvement can be made.
+
+- Laser Cutter is better than what I expected.
+
+- If the prototype shape is not complicate, Laser cutter is a good option. 
+
+- The bouncing issue is too difficult (If you only use software debouncing).
+
+- Depends on the type of button, wire, etc..., the button needed to be calibrated.
+
+  
+
+## Reference
+
+_Adafruit BME680_ (no date) _Adafruit Learning System_. Available at: [https://learn.adafruit.com/adafruit-bme680-humidity-temperature-barometic-pressure-voc-gas/overview](https://learn.adafruit.com/adafruit-bme680-humidity-temperature-barometic-pressure-voc-gas/overview) (Accessed: 21 November 2022).
+
+_BME680 Breakout - Air Quality, Temperature, Pressure, Humidity Sensor - Pimoroni_ (no date). Available at: [https://shop.pimoroni.com/products/bme680-breakout](https://shop.pimoroni.com/products/bme680-breakout) (Accessed: 21 November 2022).
+
+_Leonardo | Arduino Documentation_ (no date). Available at: [https://docs.arduino.cc/hardware/leonardo](https://docs.arduino.cc/hardware/leonardo) (Accessed: 28 November 2022).
+
+_OctoPrint Login_ (no date). Available at: [http://octopi-prusa-2.celab/login/?redirect=%2F%3F&permissions=STATUS%2CSETTINGS\_READ](http://octopi-prusa-2.celab/login/?redirect=%2F%3F&permissions=STATUS%2CSETTINGS_READ) (Accessed: 6 December 2022).
+
+_PWM conflict? One DC servo, one PWM fan motor - Using Arduino / Motors, Mechanics, Power and CNC_ (2020) _Arduino Forum_. Available at: [https://forum.arduino.cc/t/pwm-conflict-one-dc-servo-one-pwm-fan-motor/662796](https://forum.arduino.cc/t/pwm-conflict-one-dc-servo-one-pwm-fan-motor/662796) (Accessed: 3 December 2022).
+
+_UNO R3 | Arduino Documentation_ (no date). Available at: [https://docs.arduino.cc/hardware/uno-rev3](https://docs.arduino.cc/hardware/uno-rev3) (Accessed: 14 December 2022).
--- resource/main/main.ino
+++ resource/main/main.ino
@@ -1,162 +1,247 @@
 // Import library
-#include <Servo.h>
 #include <MD_Parola.h>
 #include <MD_MAX72xx.h>
 #include <SPI.h>
+#include <DHT.h>
+#include <DHT_U.h>
+// #include <Adafruit_Sensor.h>
+// #include "Adafruit_BME680.h"NF
+// #include "Zanshin_BME680.h"  // Include the BME680 Sensor library
 
-// Pinout Setting
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
 #define MAX_DEVICES 4
+
+// Pinout Setting
 #define CS_PIN    10
 #define DATA_PIN  11
 #define CLK_PIN   13
-#define buttonPin 2  // the number of the pushbutton pin
+#define buttonPin 6  // the number of the pushbutton pin
+#define infoButtonPin 8
 #define pwmPin 3    // the number of the LED pin
-#define servoPin 6 // the number of the servo motor pin
+#define DHTPin 4        // on Pin 2 of the Huzzah
+// #define servoPin 6 // the number of the servo motor pin
+
+// #define SEALEVELPRESSURE_HPA (1013.25)
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 
 // Software SPI Setting
-MD_Parola P = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
+MD_Parola P = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
+// MD_Parola P = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
+DHT dht(DHTPin, DHTTYPE);   // Initialize DHT sensor.
+// BME680_Class BME680;  ///< Create an instance of the BME680 class
+// Adafruit_BME680 bme; // I2C
 
 // LED Config
 uint8_t scrollSpeed = 25;    // default frame delay value
-textEffect_t scrollEffect = PA_NO_EFFECT;
+textEffect_t scrollEffect = PA_PRINT;
+textEffect_t scrollRemoveEffect = PA_SCROLL_LEFT;
 textPosition_t scrollAlign = PA_LEFT;
 uint16_t scrollPause = 2000; // in milliseconds
   // Global message buffers shared by Serial and Scrolling functions
-#define	BUF_SIZE	75
+#define	BUF_SIZE	70
 char curMessage[BUF_SIZE] = { "" };
 char newMessage[BUF_SIZE] = { "Hello!!!" };
+#define	Default_SIZE	50
+char defaultCurMessage[Default_SIZE] = { "" };
+char defaultNewMessage[Default_SIZE] = { "^  -" };
 bool newMessageAvailable = true;
 
 // variables Setting:
-Servo servo;
-const int servoInterval = 80;
-const int ledInterval = 80;
-const int buttonInterval = 500;
-const int servoMinDegrees = 20;
-const int servoMaxDegrees = 160;
+// Servo servo;
+// const int servoInterval = 80;
+const int ledInterval = 100;
+const int buttonInterval = 50;
+// const int servoMinDegrees = 20;
+// const int servoMaxDegrees = 160;
+const int bmeInterval = 5000;
+const int goToDefaultDisplayInterval = 10000;
 
-int buttonState = 0;  // variable for reading the pushbutton status
+int goToDefaultStatus = 0;
+int buttonState = 1;  // variable for reading the pushbutton status
+int infoButtonState = 1;
+int buttonLevel;
+int lastButtonState = 1;
+int lastInfoButtonState = 1;
 int fanSpeed = 0;
-bool servoDirection = true;
+int temp;
+int humd;
+// bool servoDirection = true;
 unsigned long currentMillis = 0;
-unsigned long previousServoMillis = 0;
+// unsigned long previousServoMillis = 0;
 unsigned long previousButtonMillis = 0;
+unsigned long infoButtonMillis = 0;
 unsigned long previousLEDMillis = 0;
-int servoDegrees = 5;
-int servoPosition = 90;
+unsigned long previousBMEMillis = 0;
+unsigned long previousDefaultDisplayMillis = 0;
+// int servoDegrees = 5;
+// int servoPosition = 90;
 
 void onPressed() {
-  if (currentMillis - previousButtonMillis >= buttonInterval){
-    if(buttonState == 10){
-      buttonState = 0;
-    }else{
-      buttonState = buttonState + 1;
-    }
-    previousButtonMillis += buttonInterval;
+  delay(30);
+  int reading = digitalRead(buttonPin);
+  // Serial.println(reading);
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    previousButtonMillis = millis();
+    // Serial.print("Time is:");
+    // Serial.println(previousButtonMillis);
   }
-  sprintf(newMessage, "Fan %d", buttonState);
-  strcpy(curMessage, newMessage);
-  newMessageAvailable = true;
+  if ((millis() - previousButtonMillis) > buttonInterval) {
+    // Serial.print("inside");
+    // Serial.print("Diif is:");
+    // Serial.println(millis() - previousButtonMillis);
+    // if the button state has changed:
+    Serial.print("reading is ");
+    Serial.print(reading);
+    Serial.print("state is ");
+    Serial.println(buttonState);
+    // only toggle the LED if the new button state is HIGH
+    if (reading == buttonState) {
+     buttonState = reading;
+      if (reading == 1) {
+        if(buttonLevel == 10){
+          buttonLevel = 0;
+        }else{
+          buttonLevel = buttonLevel + 1;
+        }
+        Serial.print("Level is ");
+        Serial.println(buttonLevel);
+        sprintf(newMessage, "Fan %d", buttonLevel);
+        strcpy(curMessage, newMessage);
+        newMessageAvailable = true;
+        previousDefaultDisplayMillis = currentMillis;
+        goToDefaultStatus = 0;
+        P.displayText(curMessage, scrollAlign, scrollSpeed, scrollPause, scrollEffect, scrollRemoveEffect);
+      }
+    lastButtonState = reading;
+    }
+  }
 }
 
-void sweepServo(){
-  if (currentMillis - previousServoMillis >= servoInterval) {
-      previousServoMillis += servoInterval;
+void onInfoBtnPressed() {
+  delay(30);
+  int reading = digitalRead(infoButtonPin);
+  if (reading != lastInfoButtonState) {
+    // reset the debouncing timer
+    infoButtonMillis = millis();
 
-      servoPosition = servoPosition + servoDegrees;
-
-      if ((servoPosition >= servoMaxDegrees) || (servoPosition <= servoMinDegrees))  {
-      servoDegrees = - servoDegrees; // reverse direction
-          // and update the position to ensure it is within range
-      servoPosition = servoPosition + servoDegrees; 
+  }
+  if ((millis() - infoButtonMillis) > buttonInterval) {
+    // only toggle the LED if the new button state is HIGH
+    if (reading == infoButtonState) {
+     infoButtonState = reading;
+      if (reading == 1) {
+        sprintf(newMessage, "Temperature : %d, Humidity : %d", temp, humd);
+        strcpy(curMessage, newMessage);
+        newMessageAvailable = true;
+        previousDefaultDisplayMillis = currentMillis;
+        goToDefaultStatus = 0;
+        P.displayText(curMessage, scrollAlign, scrollSpeed, scrollPause, scrollRemoveEffect, scrollRemoveEffect);
+      }
+    lastInfoButtonState = reading;
     }
-        // make the servo move to the next position
-    servo.write(servoPosition);
-        // and record the time when the move happened
   }
 }
 
-void displayLED(){
-  if (currentMillis - previousLEDMillis >= ledInterval) {
-    // Display
-    P.displayAnimate();
-    if (newMessageAvailable){
-      strcpy(curMessage, newMessage);
-      newMessageAvailable = false;
-      Serial.println("In display");
-      Serial.println(curMessage);
-      Serial.println(newMessage);
-    }
+// void displayLED(){
+//   if (currentMillis - previousLEDMillis >= ledInterval) {
+//     // Display
+//     previousLEDMillis += ledInterval;
+//     P.displayAnimate();
+//     if (newMessageAvailable){
+//       strcpy(curMessage, newMessage);
+//       newMessageAvailable = false;
+//       // Serial.println("In display");
+//       // Serial.println(curMessage);
+//       // Serial.println(newMessage);
+//     }
+//     P.displayReset();
+//   }
+// }
+
+void goToDefaultDisplay(){
+  if(currentMillis - previousDefaultDisplayMillis >= goToDefaultDisplayInterval && goToDefaultStatus == 0){
+    Serial.println("Going Back to Default Screen.");
+    goToDefaultStatus = 1;
+    P.displayClear();
     P.displayReset();
+    if (humd >= 80){
+      // char defaultNewMessage[Default_SIZE] = { "T   T" };
+      sprintf(defaultNewMessage, "T    T");
+      // defaultNewMessage = "T    T";
+    }
+    else if (humd >= 50){
+      // char defaultNewMessage[Default_SIZE] = { "-   -" };
+      sprintf(defaultNewMessage, "-    -");
+    }
+    else{
+      // char defaultNewMessage[Default_SIZE] = { "^   ^" };
+      sprintf(defaultNewMessage, "^     ^");
+    }
+    strcpy(defaultCurMessage, defaultNewMessage);
+    P.displayText(defaultCurMessage, PA_CENTER, scrollSpeed, 5000, PA_SCROLL_UP, PA_SCROLL_DOWN);
+  }
+  // if (currentMillis - previousDefaultDisplayMillis >= goToDefaultDisplayInterval) {
+    // Display
+    // previousDefaultDisplayMillis += goToDefaultDisplayInterval;
+}
+
+void readDHT() {
+  if (currentMillis - previousBMEMillis >= bmeInterval){
+    previousBMEMillis += bmeInterval;
+    temp = dht.readTemperature();
+    humd = dht.readHumidity();
+    Serial.print(temp);
+    Serial.print("Hum is");
+    Serial.println(humd);
   }
 }
 
 void setup() {
   // Set Serial port
   Serial.begin(9600);
+  pinMode(DHTPin, INPUT);
+  dht.begin();
+  readDHT();
   // initialize the pushbutton pin as an input and External Interrupt:
-  pinMode(buttonPin, INPUT_PULLUP);
+  // pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(buttonPin, INPUT);
+  pinMode(infoButtonPin, INPUT);
   // initialize the PWM pin as an output:
   pinMode(pwmPin, OUTPUT);
   
   // Set up external interrupt. On the event, onPressed function will executed.
   // Unlike Poll, interrup will be running in between delay.
-  attachInterrupt(digitalPinToInterrupt(buttonPin), onPressed, RISING);
+  // attachInterrupt(digitalPinToInterrupt(buttonPin), onPressed, RISING);
 
   // Set Servo
-  servo.attach(servoPin);
-  servo.write(0); //Return servo to its 0 location
+  // servo.attach(servoPin);
+  // servo.write(0); //Return servo to its 0 location
 
   // Display Init
   P.begin();
-  P.displayText(curMessage, scrollAlign, scrollSpeed, scrollPause, scrollEffect, scrollEffect);
+  P.displayText(curMessage, scrollAlign, scrollSpeed, scrollPause, scrollRemoveEffect, scrollRemoveEffect);
+  strcpy(curMessage, newMessage);
 }
 
 void loop() {
+  // Serial.println(buttonState);
   currentMillis = millis();
 
-  sweepServo();
-  displayLED();
+  // Serial.println(digitalRead(buttonPin));
+  // if(digitalRead(buttonPin) == 1){
+  //   onPressed();    
+  // }
+  // sweepServo();
+  onPressed();
+  // displayLED();
+  goToDefaultDisplay();
+  onInfoBtnPressed();
+  readDHT();
 
-  fanSpeed = buttonState * 25.5;
+  fanSpeed = buttonLevel * 25.5;
   analogWrite(pwmPin, fanSpeed);
-
-  /*
-  // Delay the loop
-  // delay(1000);
-  // Show btn state in Serial Monitor (0 - 10)
-  Serial.print("Button State is ");
-  Serial.println(buttonState);
-  // Convert btn state to fan speed(PWM) (0 - 255)
-  fanSpeed = buttonState * 25.5;
-  // Send the fan speed to teh fan
-  analogWrite(pwmPin, fanSpeed);
-  // Show the fan speed in Serial Monitor
-  Serial.print("The fan speed is ");
-  Serial.println(fanSpeed);
-
-  // Control Servo
-  if (servoDirection){
-    for (int rot = 0; rot < 180; rot++) {
-      servo.write(rot);
-      delay(10);
-    }
-    for (int rot = 180;   rot > 0; rot--) {
-      servo.write(rot);
-      delay(10); 
-    }
-  }else{
-    Serial.print("Servo Turned Off");
-  };
-
-  // Display
   if (P.displayAnimate()){
-    if (newMessageAvailable){
-      strcpy(curMessage, newMessage);
-      newMessageAvailable = false;
-    }
     P.displayReset();
   }
-  */
 }
